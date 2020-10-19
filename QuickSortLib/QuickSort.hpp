a33b6cc5d68325afeb3e7f4a46df4447dac5aac0
skiv hisink <skiv27rus@gmail.com>
2020-10-20 01:46:37 +0700

    some code clearning

--- QuickSortLib/QuickSort.hpp
+++ QuickSortLib/QuickSort.hpp
@@ -2,16 +2,21 @@
 #include <iterator>
 #include <vector>
 #include <stack>
-class QuickSort final {
+
+class QuickSort final
+{
 	QuickSort() = delete;
-	template<typename RandomAccessIterator, typename Compare = std::greater<>>
+
+	template <typename RandomAccessIterator, typename Compare = std::greater<>>
 	static int partition(RandomAccessIterator& arr, const int start_indx, const int end_indx, Compare comp)
 	{
 		auto end_indx_elem = arr[end_indx];
 		int i = (start_indx - 1);
 
-		for (int j = start_indx; j <= end_indx - 1; ++j) {
-			if (!comp(arr[j], end_indx_elem)) {
+		for (int j = start_indx; j <= end_indx - 1; ++j)
+		{
+			if (!comp(arr[j], end_indx_elem))
+			{
 				++i;
 				std::swap(arr[i], arr[j]);
 			}
@@ -19,29 +24,34 @@ class QuickSort final {
 		std::swap(arr[i + 1], arr[end_indx]);
 		return (i + 1);
 	}
+
 public:
-	template<typename RandomAccessIterator, typename Compare = std::greater<>>
+	template <typename RandomAccessIterator, typename Compare = std::greater<>>
 	static void Qsort(RandomAccessIterator begin, size_t elements, Compare comp = Compare())
 	{
 		std::stack<int> stack;
 		int start_indx = 0;
 		int end_indx = elements - 1;
-		if (start_indx > end_indx) {
+		if (start_indx > end_indx)
+		{
 			return;
 		}
 		stack.push(start_indx);
 		stack.push(end_indx);
-		while (!stack.empty()) {
+		while (!stack.empty())
+		{
 			end_indx = stack.top();
 			stack.pop();
 			start_indx = stack.top();
 			stack.pop();
 			int pivot = partition(begin, start_indx, end_indx, comp);
-			if (pivot - 1 > start_indx) {
+			if (pivot - 1 > start_indx)
+			{
 				stack.push(start_indx);
 				stack.push(pivot - 1);
 			}
-			if (pivot + 1 < end_indx) {
+			if (pivot + 1 < end_indx)
+			{
 				stack.push(pivot + 1);
 				stack.push(end_indx);
 			}
@@ -49,8 +59,9 @@ public:
 	}
 };
 
-namespace quick_sort {
-	template<class iter>
+namespace quick_sort
+{
+	template <class iter>
 	using it_tag = typename std::iterator_traits<iter>::iterator_category;
 
 	template <typename RandomAccessIterator, typename Compare = std::greater<>,
